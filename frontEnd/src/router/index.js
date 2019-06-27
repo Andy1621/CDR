52c3a744a5c5a105695e7860d64523a12c2701c8
HuQi <39493195+qzkrc567@users.noreply.github.com>
2019-06-27 18:04:33 +0800

    Revert "登录注册页面实现"
    This reverts commit 89dbf711299295586fb2b354a825fa01bd34b917.

--- frontEnd/src/router/index.js
+++ frontEnd/src/router/index.js
@@ -1,21 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from '@/pages/index'
-import login from '@/pages/login'
+import index from '@/components/index'
 
 Vue.use(Router)
 
 export default new Router({
-    routes: [
+  routes: [
     {
-        path: '/',
-        name: 'login',
-        component: login
-    },
-    {
-        path: '/index',
-        name: 'index',
-        component: index
+      path: '/',
+      name: 'index',
+      component: index
     }
   ]
 })
