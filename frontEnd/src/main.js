900c065951732daaa6b45763b06c1838593a2ef9
HuQi <39493195+qzkrc567@users.noreply.github.com>
2019-06-27 14:28:08 +0800

    引入iview

--- frontEnd/src/main.js
+++ frontEnd/src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
 
-Vue.config.productionTip = false
+Vue.use(iView);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +16,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
