062e23441d795316573975af4b3e15afd63719d6
tygkking <tygkking@163.com>
2019-06-28 11:23:16 +0800

    Create apply.vue
    作品提交信息填写页面

---
+++ frontEnd/src/pages/apply.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="body">
+        <h1>作品提交内容填写</h1>
+        <Steps :current="current" style="margin: 30px">
+            <Step title="作品基本信息" content=""></Step>
+            <Step title="申请者信息" content=""></Step>
+            <Step title="作品详细信息" content=""></Step>
+            <Step title="附件上传" content=""></Step>
+        </Steps>
+        <Divider/>
+        <div class="form" v-show="current == 0">
+            <Form :model="project" :label-width="80">
+
+            </Form>
+            <Row>
+                <Col span="10">作品名称</Col>
+                <Col span="13" offset="1">
+
+                </Col>
+            </Row>
+            <Row>
+                <Col span="10">院系名称</Col>
+                <Col span="13" offset="1">
+
+                </Col>
+            </Row>
+            <Row>
+                <Col span="10">类别</Col>
+                <Col span="13" offset="1">
+
+                </Col>
+            </Row>
+        </div>
+        <div class="form" v-show="current == 1">
+            <Row>
+                <Col span="10">姓名</Col>
+                <Col span="13" offset="1">
+
+                </Col>
+            </Row>
+        </div>
+        <div class="form" v-show="current == 2">
+            <Row>
+                <Col span="10">作品全称</Col>
+                <Col span="13" offset="1">
+
+                </Col>
+            </Row>
+        </div>
+        <div class="form" v-show="current == 3">
+            <Row>
+                <Col span="10">图片</Col>
+                <Col span="13" offset="1">
+
+                </Col>
+            </Row>
+        </div>
+        <Button type="primary" :disabled="current == 0" @click="pre_step">上一步</Button>
+        <Button type="primary" :disabled="current == 3" @click="next_step">下一步</Button>
+        <br>
+        <Button type="primary" @click="save_data">保存</Button>
+        <Button type="success" v-show="current == 3">提交</Button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "apply",
+        data(){
+            return{
+                current: 0,
+            }
+        },
+        methods:{
+            next_step(){
+                this.current += 1;
+                this.save_data();
+            },
+            pre_step(){
+                this.current -= 1;
+                this.save_data();
+            },
+            save_data(){
+                // save data function
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .body{
+        margin-left: 24%;
+        border: 1px dashed black;
+        margin-top: 60px;
+        padding: 20px;
+        width: 50%;
+        text-align: center;
+        min-height: 470px;
+        min-width: 600px;
+        color: black;
+    }
+    .form{
+        min-height: 300px;
+        padding: 10px;
+        margin-bottom: 30px;
+        font-size: 15px;
+    }
+</style>
