c0f9c8498b56833e69ddb41d9d052b34080b14c2
HuQi <39493195+qzkrc567@users.noreply.github.com>
2019-06-29 17:55:38 +0800

    新加页面的侧边栏

--- frontEnd/src/pages/competitionList.vue
+++ frontEnd/src/pages/competitionList.vue
@@ -1,15 +1,42 @@
 <template>
     <div>
         <NavBar></NavBar>
+        <div class="body">
+            <h2>我的项目</h2>
+            <Table stripe border :columns="columns" :data="rows" ref="table" ></Table>
+        </div>
     </div>
 </template>
 
 <script>
+    import NavBar from '../components/NavBar.vue'
     export default {
-        name: "competitionList"
+        components:{
+            NavBar
+        },
+        name: 'competitionList',
+        data () {
+            return {
+
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .body{
+        left: 280px;
+        bottom: 50px;
+        position: relative;
+        top: 100px;
+        border: 1px dashed black;
+        border-radius: 20px;
+        /*margin: 100px 0px 50px 30%;*/
+        padding: 20px;
+        width: 75%;
+        /*text-align: center;*/
+        min-height: 470px;
+        min-width: 600px;
+        color: black;
+    }
 </style>
