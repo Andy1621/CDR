210153b1b6c9b6237fc46dcffd4b79f226a26267
HuQi <39493195+qzkrc567@users.noreply.github.com>
2019-06-30 14:21:54 +0800

    Update index.js

--- frontEnd/src/router/index.js
+++ frontEnd/src/router/index.js
@@ -9,6 +9,8 @@ import addCompetition from '@/pages/addCompetition'
 import announce from '@/pages/announce'
 import competitionList from '@/pages/competitionList'
 import professorManagement from '@/pages/professorManagement'
+import projectReview from '@/pages/projectReview'
+import projectDetail from '@/pages/projectDetail'
 Vue.use(Router);
 
 export default new Router({
@@ -58,6 +60,16 @@ export default new Router({
             path: '/professorManagement',
             name: 'professorManagement',
             component: professorManagement,
+        },
+        {
+            path: '/projectReview',
+            name: 'projectReview',
+            component: projectReview,
+        },
+        {
+            path: '/projectDetail',
+            name: 'projectDetail',
+            component: projectDetail,
         },
   ]
 })
