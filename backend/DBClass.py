2f91e2baeea213af198b18eaed9de1159383a994
AyakashiQ <julytony@163.com>
2019-07-01 15:53:41 +0900

    Update DBClass.py

--- backend/DBClass.py
+++ backend/DBClass.py
@@ -592,7 +592,7 @@ class DbOperate:
             # refuse_addr = "<a href=\"" + refuse_addr + "\">" + refuse_addr + "</a>"
             refuse_addr = "<a href=\"" + refuse_addr + "\">" + "拒绝评审" + "</a>"
             message = "<p>尊敬的 " + expert_name + " 先生/女士您好，\n</p>" + \
-                      "<p>" + comp_name + "组委会诚邀您参与参赛项目：\"" + project_name + "\"的评审工作。\n</p>" + \
+                      "<p>" + comp_name + "组委会诚邀您参与参赛项目\"" + project_name + "\"的评审工作。\n</p>" + \
                       "<p>如果您接受此邀请，请点击链接: " + accept_addr + " 进入竞赛系统。\n</p>" + \
                       "<p>如果您希望拒绝此邀请，请点击链接: " + refuse_addr + "确认拒绝。\n</p>" + \
                       "<p>衷心感谢您的付出和支持。\n</p>" + \
