4216980c4858be8444c68f13cd0ccecbb28d9c70
tygkking <tygkking@163.com>
2019-07-04 13:09:56 +0800

    发布竞赛页面完成

--- frontEnd/src/pages/addCompetition.vue
+++ frontEnd/src/pages/addCompetition.vue
@@ -4,79 +4,129 @@
         <div class="body">
             <h2>添加竞赛</h2>
             <div class="form">
-                <Form ref="competition" :model="competitionInfo" :rules="ruleCompetitionInfo" :label-width="120">
+                <Form ref="competition" :model="competitionInfo" :rules="ruleCompetitionInfo" :label-width="100">
                     <FormItem label="竞赛名称" prop="competition_name">
                         <Input v-model="competitionInfo.competition_name" placeholder="请输入竞赛名称"></Input>
                     </FormItem>
-                    <FormItem label="开始时间">
-                        <DatePicker type="date" placeholder="日期选择" v-model="competitionInfo.begin_time_date"
-                                    prop="begin_time_date"></DatePicker>
-                        <TimePicker format="HH:mm" placeholder="时间选择" v-model="competitionInfo.begin_time_time"
-                                    prop="begin_time_time"></TimePicker>
-                    </FormItem>
-                    <FormItem label="提交截止">
-                        <DatePicker type="date" placeholder="日期选择" v-model="competitionInfo.submission_ddl_date"
-                                    prop="submission_ddl_date"></DatePicker>
-                        <TimePicker format="HH:mm" placeholder="时间选择" v-model="competitionInfo.submission_ddl_time"
-                                    prop="submission_ddl_time"></TimePicker>
-                    </FormItem>
-                    <FormItem label="初审截止">
-                        <DatePicker type="date" placeholder="日期选择" v-model="competitionInfo.first_review_ddl_date"
-                                    prop="first_review_ddl_date"></DatePicker>
-                        <TimePicker format="HH:mm" placeholder="时间选择" v-model="competitionInfo.first_review_ddl_time"
-                                    prop="first_review_ddl_time"></TimePicker>
-                    </FormItem>
-                    <FormItem label="专家初评截止">
-                        <DatePicker type="date" placeholder="日期选择" v-model="competitionInfo.expert_comments_ddl_date"
-                                    prop="expert_comments_ddl_date"></DatePicker>
-                        <TimePicker format="HH:mm" placeholder="时间选择" v-model="competitionInfo.expert_comments_ddl_time"
-                                    prop="expert_comments_ddl_time"></TimePicker>
-                    </FormItem>
-                    <FormItem label="现场赛选拔截止">
-                        <DatePicker type="date" placeholder="日期选择" v-model="competitionInfo.live_selection_ddl_date"
-                                    prop="live_selection_ddl_date"></DatePicker>
-                        <TimePicker format="HH:mm" placeholder="时间选择" v-model="competitionInfo.live_selection_ddl_time"
-                                    prop="live_selection_ddl_time"></TimePicker>
-                    </FormItem>
-                    <FormItem label="结束时间">
-                        <DatePicker type="date" placeholder="日期选择" v-model="competitionInfo.end_time_date"
-                                    prop="end_time_date"></DatePicker>
-                        <TimePicker format="HH:mm" placeholder="时间选择" v-model="competitionInfo.end_time_time"
-                                    prop="end_time_time"></TimePicker>
-                    </FormItem>
+                    <Row>
+                        <Col span="12">
+                            <FormItem label="开始时间">
+                                <Row>
+                                    <Col span="12">
+                                        <FormItem prop="begin_time_date">
+                                            <DatePicker type="date" placeholder="日期选择"
+                                                        v-model="competitionInfo.begin_time_date"></DatePicker>
+                                        </FormItem>
+                                    </Col>
+                                    <Col span="12">
+                                        <FormItem prop="begin_time_time">
+                                            <TimePicker format="HH:mm" placeholder="时间选择"
+                                                        v-model="competitionInfo.begin_time_time"></TimePicker>
+                                        </FormItem>
+                                    </Col>
+                                </Row>
+                            </FormItem>
+                        </Col>
+                        <Col span="12">
+                            <FormItem label="提交截止">
+                                <Row>
+                                    <Col span="12">
+                                        <FormItem prop="submission_ddl_date">
+                                            <DatePicker type="date" placeholder="日期选择"
+                                                        v-model="competitionInfo.submission_ddl_date"></DatePicker>
+                                        </FormItem>
+                                    </Col>
+                                    <Col span="12">
+                                        <FormItem prop="submission_ddl_time">
+                                            <TimePicker format="HH:mm" placeholder="时间选择"
+                                                        v-model="competitionInfo.submission_ddl_time"></TimePicker>
+                                        </FormItem>
+                                    </Col>
+                                </Row>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span="12">
+                            <FormItem label="初审截止">
+                                <Row>
+                                    <Col span="12">
+                                        <FormItem prop="first_review_ddl_date">
+                                            <DatePicker type="date" placeholder="日期选择"
+                                                        v-model="competitionInfo.first_review_ddl_date"></DatePicker>
+                                        </FormItem>
+                                    </Col>
+                                    <Col span="12">
+                                        <FormItem prop="first_review_ddl_time">
+                                            <TimePicker format="HH:mm" placeholder="时间选择"
+                                                        v-model="competitionInfo.first_review_ddl_time"></TimePicker>
+                                        </FormItem>
+                                    </Col>
+                                </Row>
+                            </FormItem>
+                        </Col>
+                        <Col span="12">
+                            <FormItem label="专家初评截止">
+                                <Row>
+                                    <Col span="12">
+                                        <FormItem prop="expert_comments_ddl_date">
+                                            <DatePicker type="date" placeholder="日期选择"
+                                                        v-model="competitionInfo.expert_comments_ddl_date"></DatePicker>
+                                        </FormItem>
+                                    </Col>
+                                    <Col span="12">
+                                        <FormItem prop="expert_comments_ddl_time">
+                                            <TimePicker format="HH:mm" placeholder="时间选择"
+                                                        v-model="competitionInfo.expert_comments_ddl_time"></TimePicker>
+                                        </FormItem>
+                                    </Col>
+                                </Row>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span="12">
+                            <FormItem label="现场赛选拔截止">
+                                <Row>
+                                    <Col span="12">
+                                        <FormItem prop="live_selection_ddl_date">
+                                            <DatePicker type="date" placeholder="日期选择"
+                                                        v-model="competitionInfo.live_selection_ddl_date"></DatePicker>
+                                        </FormItem>
+                                    </Col>
+                                    <Col span="12">
+                                        <FormItem prop="live_selection_ddl_time">
+                                            <TimePicker format="HH:mm" placeholder="时间选择"
+                                                        v-model="competitionInfo.live_selection_ddl_time"></TimePicker>
+                                        </FormItem>
+                                    </Col>
+                                </Row>
+                            </FormItem>
+                        </Col>
+                        <Col span="12">
+                            <FormItem label="结束时间">
+                                <Row>
+                                    <Col span="12">
+                                        <FormItem prop="end_time_time">
+                                            <DatePicker type="date" placeholder="日期选择"
+                                                        v-model="competitionInfo.end_time_date"></DatePicker>
+                                        </FormItem>
+                                    </Col>
+                                    <Col span="12">
+                                        <FormItem prop="end_time_time">
+                                            <TimePicker format="HH:mm" placeholder="时间选择"
+                                                        v-model="competitionInfo.end_time_time"></TimePicker>
+                                        </FormItem>
+                                    </Col>
+                                </Row>
+                            </FormItem>
+                        </Col>
+                    </Row>
                     <FormItem label="竞赛简介" prop="introduction">
                         <Input v-model="competitionInfo.introduction" type="textarea" :maxlength="800" :rows="6"
                                :autosize="{minRows: 6,maxRows: 6}" placeholder="请输入竞赛简介（不超过800字）"></Input>
                     </FormItem>
                 </Form>
-            </div>
-            <Divider/>
-            <div style="margin-left: 50px">
-                <h5 style="margin-bottom: 20px">附件上传</h5>
-                <div v-if="readonly" class="show-upload">
-                    <ul>
-                        <li v-for="item in uploadDocList" @click="docView(item)">
-                            <Icon type="md-document"></Icon>
-                            {{item.name}}
-                        </li>
-                    </ul>
-                </div>
-                <Upload
-                        ref="uploadDoc"
-                        v-show="!readonly"
-                        :show-upload-list="true"
-                        :default-file-list="defaultDocList"
-                        :on-success="handleSuccessDoc"
-                        :on-remove="handleRemoveDoc"
-                        :on-preview="docView"
-                        :max-size="20480"
-                        :on-format-error="handleFormatErrorDoc"
-                        :on-exceeded-size="handleMaxSizeDoc"
-                        :before-upload="handleBeforeUploadDoc"
-                        :action="up_url"
-                        style="display: inline-block">
-                    <Button icon="ios-document" style="width: 100px">上传</Button>
-                </Upload>
             </div>
             <Button type="primary" @click="add_competition" style="margin-left: 45%">新建竞赛</Button>
         </div>
@@ -158,6 +208,7 @@
         },
         methods: {
             add_competition() {
+                // this.format_change(this.competitionInfo.begin_time_date, this.competitionInfo.begin_time_time)
                 this.$refs['competition'].validate((valid) => {
                     if (valid) {
                         let params = {
@@ -168,6 +219,7 @@
                             'expert_comments_ddl': this.format_change(this.competitionInfo.expert_comments_ddl_date, this.competitionInfo.expert_comments_ddl_time),
                             'live_selection_ddl': this.format_change(this.competitionInfo.live_selection_ddl_date, this.competitionInfo.live_selection_ddl_time),
                             'end_time': this.format_change(this.competitionInfo.end_time_date, this.competitionInfo.end_time_time),
+                            'introduction': this.competitionInfo.introduction
                         }
                         this.$http.post(this.$baseURL + '/api/v1/add_competition', params)
                             .then(function (res) {
@@ -190,7 +242,11 @@
                 });
             },
             format_change(date, time) {
-                var res = date.toString() + time.toString()
+                var nowdate = new Date(date)
+                var rightdate = nowdate.getFullYear() + '-' + (nowdate.getMonth()+1) + '-' + nowdate.getDate()
+                time = time+':00'
+                var res = rightdate + ' ' + time
+                console.log(res)
                 return res
             },
         },
