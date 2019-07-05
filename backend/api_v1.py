f4472cf6283829eef426726737de9614e0ebc502
Alola-Kirby <1796650517@qq.com>
2019-07-05 09:39:36 +0800

    小修改

--- backend/api_v1.py
+++ backend/api_v1.py
@@ -1045,7 +1045,7 @@ class EnterDefenseList(Resource):
         res = {"state": "fail"}
         try:
             data = request.get_json()
-            lists = data.get('list')
+            lists = data.get('projlst')
             res = db.enter_defense_list(lists)
         except:
             pass
